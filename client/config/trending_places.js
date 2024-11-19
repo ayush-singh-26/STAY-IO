09269024dfb6f11aaaf3f5027f5d6a36a1b148bb
ayush-singh-225 <ayushsinghtemp@gmail.com>
2024-11-20 00:47:09 +0530

    facility section added

---
+++ client/config/trending_places.js
@@ -0,0 +1,35 @@
+export const trending_places=[
+    {
+        place:'Delhi',
+        description:'The city of lights',
+        image:'./images/places/delhi.jpg'
+    },
+    {
+        place:'Mumbai',
+        description:'The city of dreams',
+        image:'./images/places/mumbai.jpg'
+    },
+    {
+        place:'Hydrabad ',
+        description:'The city of bustling',
+        image:'./images/places/hydrabad.jpg'
+    },
+    {
+        place:'Vasco Da Gama',
+        description:'The city of love',
+        image:'./images/places/vasco de gama.jpg'
+    },
+    {
+        place:'Gujrat',
+        description:'The city of innovation',
+        image:'./images/places/gujrat.jpg'
+    },
+    {
+        place:'Goa',
+        description:'The city of sustainability',
+        image:'./images/places/goa.jpg'
+    },
+    
+    
+
+]
