810c7ee97f37eb907badd65d5d04bf6a304c3a0e
ayush-singh-225 <ayushsinghtemp@gmail.com>
2025-04-25 00:01:02 +0530

    file name casing change

--- client/src/pages/Hotel_details.jsx
+++ client/src/pages/Hotel_details.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import Comment from "../components/comment";
+import Comment from "../components/Comment";
 
 function Hotel_details() {
   const navigate = useNavigate();
