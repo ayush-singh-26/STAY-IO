f8d732db5783351c65ea3f995d9261e6d0734813
ayush-singh-225 <ayushsinghtemp@gmail.com>
2025-09-03 01:12:15 +0530

    image location changed

--- client/config/trending_places.js
+++ client/config/trending_places.js
@@ -2,32 +2,32 @@ export const trending_places=[
     {
         place:'Delhi',
         description:'The city of lights',
-        image:'./images/places/delhi.jpg'
+        image:'./public/images/places/delhi.jpg'
     },
     {
         place:'Mumbai',
         description:'The city of dreams',
-        image:'./images/places/mumbai.jpg'
+        image:'./public/images/places/mumbai.jpg'
     },
     {
         place:'Hydrabad ',
         description:'The city of bustling',
-        image:'./images/places/hydrabad.jpg'
+        image:'./public/images/places/hydrabad.jpg'
     },
     {
         place:'Vasco Da Gama',
         description:'The city of love',
-        image:'./images/places/vasco de gama.jpg'
+        image:'./public/images/places/vasco de gama.jpg'
     },
     {
         place:'Gujrat',
         description:'The city of innovation',
-        image:'./images/places/gujrat.jpg'
+        image:'./public/images/places/gujrat.jpg'
     },
     {
         place:'Goa',
         description:'The city of sustainability',
-        image:'./images/places/goa.jpg'
+        image:'./public/images/places/goa.jpg'
     },
     
     
